6bd9d96fd79ef3ac34c89f2df2b493bbb2ca5269
ButterButterman <butterbuttermandev@gmail.com>
2022-05-15 01:37:15 -0400

    committing this note before i forget

--- mac/setup.sh
+++ mac/setup.sh
@@ -2,6 +2,8 @@
 
 echo "doing things..."
 
+#check if homebrew is installed, if not then install it here
+
 #call brew installer (it needs to continue if failure)
 
 #setup configs (vim/ohmyzsh/vscode/etc)
