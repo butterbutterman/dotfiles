3b035f97be8604ade3e18fac512e4033c601c9c4
ButterButterman <butterbuttermandev@gmail.com>
2022-05-15 01:54:40 -0400

    added spacemacs and vimrc installtion and config

--- mac/setup.sh
+++ mac/setup.sh
@@ -6,4 +6,17 @@ echo "doing things..."
 
 #call brew installer (it needs to continue if failure)
 
+echo "Installing spacemacs..."
+git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
+cp dotfiles/spacemacs ~/.spacemacs
+echo "Spacemacs installed!"
+
 #setup configs (vim/ohmyzsh/vscode/etc)
+
+echo "Installing ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "ohmyzsh installed! Oh my!"
+
+echo "Installing Vim configurations..."
+cp dotfiles/vimrc ~/.vimrc
+echo "Vim configurations installed!"
