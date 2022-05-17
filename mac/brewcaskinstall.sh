7776117f6bbd09ebb89a5be60c5edb9c93702fbc
ButterButterman <butterbuttermandev@gmail.com>
2022-05-16 23:13:23 -0400

    add bitwarden and todo on cask install script

--- mac/brewcaskinstall.sh
+++ mac/brewcaskinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
+#do something to skip failed casks to just write this as a foreach loop in bash
+#split sha and non sha loops and handle failures
 brew install --cask $(cat dotfiles/casks.txt)
